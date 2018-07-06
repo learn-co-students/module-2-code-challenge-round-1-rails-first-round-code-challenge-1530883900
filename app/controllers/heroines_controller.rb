0477604edb92e5ac1c7a95c37d075cc957ef1e7c
github username <github email address>
2018-07-06 11:02:54 -0400

    Done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,30 @@
 class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
+    # @powers = Power.all
+    # @powers = Power.search(params[:search])
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to @heroine
+    else
+      render :new
+    end
+  end
+
+  private
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
   end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,28 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def new
+    @power = Power.new
+  end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+  
+  def create
+    @power = Power.new(power_params)
+    if @power.save
+      redirect_to @power
+    else
+      @powers = Power.all
+      render :new
+    end
+  end
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
 end
