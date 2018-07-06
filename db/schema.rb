a32f69f5229741b79dac1e690d07387b116d12b6
Wolfgang Pfretzschner <wolfgangp@live.com>
2018-07-06 10:58:56 -0400

    done

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170725185638) do
+ActiveRecord::Schema.define(version: 20180706134540) do
 
   create_table "heroines", force: :cascade do |t|
-    t.string   "name"
-    t.string   "super_name"
+    t.string "name"
+    t.string "super_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "power_id"
+    t.index ["power_id"], name: "index_heroines_on_power_id"
   end
 
   create_table "powers", force: :cascade do |t|
-    t.string   "name"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "heroine_id"
+    t.index ["heroine_id"], name: "index_powers_on_heroine_id"
   end
 
 end
