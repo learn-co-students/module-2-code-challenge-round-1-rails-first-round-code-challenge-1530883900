a0eb9a1299543f4b00e67cdb42819397e3d2108b
github username <github email address>
2018-07-06 11:02:26 -0400

    all done

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,35 @@
 class HeroinesController < ApplicationController
+  before_action :set_heroine, only: :show
+
   def index
     @heroines = Heroine.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :power_id, :super_name)
+  end
+
+  def set_heroine
+    @heroine = Heroine.find(params[:id])
   end
 end
--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,6 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+
+  validates :super_name, uniqueness: {message: "Heroine super name must be unique"}
+  validates :power_id, presence: true
 end
