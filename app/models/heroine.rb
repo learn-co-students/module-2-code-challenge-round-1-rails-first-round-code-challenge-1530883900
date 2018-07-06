88d28a644c2286dc76ab5f8b070bc224f7e8c777
Risher Randall <randallr18@mail.wlu.edu>
2018-07-06 11:01:33 -0400

    half way der

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,11 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+  validates :super_name, uniqueness: true
+
+  # def self.search(search)
+  #   strnegth_id = Strnegth.find_by(name: search)
+  #   self.where("s") WHOO KNOWWS
+  # end
+
+  
 end
