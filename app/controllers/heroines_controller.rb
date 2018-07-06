4749a0ea9d00da649b2fb844cdd95d49ca5378e8
Shing Wong <shing2487@gmail.com>
2018-07-06 10:35:36 -0400

    finished all required deliverables

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,37 @@
 class HeroinesController < ApplicationController
+  before_action :get_heroine, only: :show
+
   def index
     @heroines = Heroine.all
+    @powers = Power.all
   end
+
+  def show
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to @heroine
+    else
+      flash[:alert] = "The Super heroine name is already taken, please try again."
+      render :new
+    end
+  end
+
+  private
+
+  def get_heroine
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end
